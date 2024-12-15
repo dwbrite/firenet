19772d371cbf5d088f8a5dc06d6134596b099e1d
Devin Brite <devin@dwbrite.com>
2024-12-15 15:13:50 -0500

    add minecraft server

---
+++ terraform/2-applications/minecraft.tf
@@ -0,0 +1,95 @@
+resource "kubernetes_namespace" "minecraft" {
+  metadata {
+    name = "minecraft"
+    labels = {
+      istio-injection = "enabled"
+    }
+  }
+}
+
+resource "kubernetes_manifest" "argocd_application_minecraft" {
+  manifest = {
+    apiVersion = "argoproj.io/v1alpha1"
+    kind       = "Application"
+    metadata = {
+      name      = "minecraft-server"
+      namespace = "argocd" # Adjust if your ArgoCD namespace is different
+    }
+    spec = {
+      project = "default"
+      source = {
+        repoURL        = "https://itzg.github.io/minecraft-server-charts/"
+        chart          = "minecraft"
+        targetRevision = "4.23.6"
+        helm = {
+          values = <<-EOF
+            minecraftServer:
+              eula: "true"
+              type: "VANILLA"
+              version: "LATEST"
+              memory: "4G"
+              difficulty: "normal"
+              maxPlayers: 50
+              gameMode: "survival"
+              motd: "Welcome to Devin's Minecraft server!"
+
+            persistence:
+              enabled: true
+              size: 50Gi
+              accessModes:
+                - ReadWriteOnce
+
+            resources:
+              requests:
+                memory: "4096Mi"
+                cpu: "2000m"
+          EOF
+        }
+      }
+      destination = {
+        server    = "https://kubernetes.default.svc"
+        namespace = kubernetes_namespace.minecraft.metadata[0].name
+      }
+      syncPolicy = {
+        automated = {
+          selfHeal = true
+          prune    = true
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_manifest" "vservice_minecraft" {
+  manifest = {
+    apiVersion = "networking.istio.io/v1alpha3"
+    kind       = "VirtualService"
+    metadata = {
+      name      = "vservice-minecraft"
+      namespace = kubernetes_namespace.minecraft.metadata[0].name
+    }
+    spec = {
+      hosts = ["minecraft.dwbrite.com", "minecraft.tiny.pizza"]
+      gateways = ["istio-system/dwbrite-com-gateway", "istio-system/tiny-pizza-gateway"]
+      tcp = [
+        {
+          match = [
+            {
+              port = 1337
+            }
+          ]
+          route = [
+            {
+              destination = {
+                host = "minecraft-server-minecraft"
+                port = {
+                  number = 25565
+                }
+              }
+            }
+          ]
+        }
+      ]
+    }
+  }
+}
